813c62704cc22fd91aa6199d64dd2ef642c62af2
Karolaith Orozco <korozco@wawand.co>
2021-01-13 15:05:19 -0500

    first commit

--- AppReloj/JS/AppUI.js
+++ AppReloj/JS/AppUI.js
@@ -1,49 +1,58 @@
 // changeLayout Muestra verticalmente  
 function changeLayout() {
-   var hours = document.getElementById("input-hour");
-   var minutes = document.getElementById("input-minute");
- 
-   hours.classList.remove("col-sm-6");
-   hours.classList.add("col-sm-12");
-   minutes.classList.remove("col-sm-6");
-   minutes.classList.add("col-sm-12");
- 
+  var hours = document.getElementById("input-hour");
+  var minutes = document.getElementById("input-minute");
+
+  if(hours.classList.contains('col-sm-6')){
+    hours.classList.remove("col-sm-6");
+    hours.classList.add("col-sm-12");
+    minutes.classList.remove("col-sm-6");
+    minutes.classList.add("col-sm-12");
+ }else{
+    if(hours.classList.contains('col-sm-12')){
+        hours.classList.remove("col-sm-12");
+        hours.classList.add("col-sm-6");
+        minutes.classList.remove("col-sm-12");
+        minutes.classList.add("col-sm-6");
+    }
  }
- 
- // Hallar angulo 
- 
+
+}
+
+// Hallar angulo 
+
 function sendInformation() {
+
   var hours = document.getElementById("hours").value;
   var minutes = document.getElementById("minutes").value;
   var textSecondary = document.getElementById("text-secondary");
   var textInitial = document.getElementById("text-initial");
   var inputError = document.getElementById("input-error");
- 
-  // Muestra si hay un error en los numeros digitados 
+
+ // Muestra si hay un error en los numeros digitados 
   if(! inputError.classList.contains('d-none')){
-    inputError.classList.add('d-none');
-  }
- 
+   inputError.classList.add('d-none');
+ }
+
   // Validate error
   if (hours > 12) {
     return inputError.classList.remove ("d-none")
   }
- 
+
   if (minutes > 59) {
     return inputError.classList.remove ("d-none")
   }
- 
+
   var angle = (30 * hours) - ((11 / 2) * minutes);
   console.log(angle);
- 
+
   // Process angle
   document.getElementById("angle").innerHTML = angle + "º"
   // Mostrar las horas y minutos con las que se hace el angulo
   document.getElementById("hour").innerHTML = hours + ' Hours'
   document.getElementById("minute").innerHTML = minutes + ' Minutes'
- 
+
   textInitial.classList.add("d-none");
   textSecondary.classList.remove ("d-none");
 }
- 
- 
+
